85ce96f4029896a16c874f2f1ab804dbf0f079ef
msuhaib01 <127488926+msuhaib01@users.noreply.github.com>
2023-07-14 06:13:17 +0500

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -60,9 +60,6 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
-# ... place you code here to LIST accounts ...
-
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -78,8 +75,6 @@ def list_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
-# ... place you code here to READ an account ...
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
